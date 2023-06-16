b4f3047532a5ac823286880f8058dc5472fe2d0d
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 14:57:00 +0700

    ubah koneksi ke db dev

--- supports/connection.php
+++ supports/connection.php
@@ -1,5 +1,5 @@
 <?php
-  	$connection = new mysqli("localhost", "sinargrafindo_itsg", "waesakitsg1980", "sinargrafindo_dabs");
+  	$connection = new mysqli("localhost", "sinargrafindo_itsg", "waesakitsg1980", "sinargrafindo_dabsdev");
   
   	if ($connection->connect_error) 
   		{ die("ERROR: Database connection failed : " . $connection->connect_error); } 
