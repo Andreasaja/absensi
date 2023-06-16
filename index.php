79701dfe6e92207e3c065d158af616c0d089d8c3
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 14:47:46 +0700

    cek versioning

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>Form Login</title>
 </head>
 <body class="text-center"><br><br><br><br>
-    <center><h1 class="h3 mb-3 font-weight-normal">ABSENSI KARYAWAN</h1><br>Last Update : 04032021</center><br><br>
+    <center><h1 class="h3 mb-3 font-weight-normal">ABSENSI KARYAWAN VERSIONING</h1><br>Last Update : 04032021</center><br><br>
     <!--   <center><h4>Under Maintenance till 11.00 WIB, please don't use this web project.</h3></center>       -->
     <div class="login-card">
         <h1>FORM LOGIN</h1><br>
